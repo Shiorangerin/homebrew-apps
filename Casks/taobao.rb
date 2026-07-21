5f83675d0ccdb0807f94ae2bf537be13f8eec92a
Orangerin <orangerin@OrangerindeMacBook-Air.local>
2026-07-21 17:30:35 +0800

    taobao: fix style lint

--- Casks/taobao.rb
+++ Casks/taobao.rb
@@ -5,19 +5,21 @@ cask "taobao" do
   url "file:///Users/orangerin/Downloads/taobao-setup-darwin-arm64-2.5.1.dmg"
   name "淘宝桌面版"
   desc "淘宝桌面版客户端"
-  homepage "https://www.taobao.com"
+  homepage "https://www.taobao.com/"
+
+  depends_on :macos
 
   app "淘宝桌面版.app"
 
   zap trash: [
-    "~/Library/Application Support/taobao-desktop",
     "~/Library/Application Support/com.taobao.desktop",
-    "~/Library/Preferences/com.taobao.desktop.plist",
+    "~/Library/Application Support/taobao-desktop",
     "~/Library/Caches/com.taobao.desktop",
     "~/Library/Caches/taobao-desktop",
-    "~/Library/Saved Application State/com.taobao.desktop.savedState",
-    "~/Library/Logs/com.taobao.desktop",
     "~/Library/HTTPStorages/com.taobao.desktop",
+    "~/Library/Logs/com.taobao.desktop",
+    "~/Library/Preferences/com.taobao.desktop.plist",
+    "~/Library/Saved Application State/com.taobao.desktop.savedState",
     "~/Library/WebKit/com.taobao.desktop",
   ]
 end
