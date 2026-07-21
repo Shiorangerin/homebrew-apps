80fbb2f19b25ade7911511bbac93ec7b97914718
Orangerin <orangerin@OrangerindeMacBook-Air.local>
2026-07-21 17:27:38 +0800

    taobao: add zap directives for clean uninstall

--- Casks/taobao.rb
+++ Casks/taobao.rb
@@ -8,4 +8,16 @@ cask "taobao" do
   homepage "https://www.taobao.com"
 
   app "淘宝桌面版.app"
+
+  zap trash: [
+    "~/Library/Application Support/taobao-desktop",
+    "~/Library/Application Support/com.taobao.desktop",
+    "~/Library/Preferences/com.taobao.desktop.plist",
+    "~/Library/Caches/com.taobao.desktop",
+    "~/Library/Caches/taobao-desktop",
+    "~/Library/Saved Application State/com.taobao.desktop.savedState",
+    "~/Library/Logs/com.taobao.desktop",
+    "~/Library/HTTPStorages/com.taobao.desktop",
+    "~/Library/WebKit/com.taobao.desktop",
+  ]
 end
