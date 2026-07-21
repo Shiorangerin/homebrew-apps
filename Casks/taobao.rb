301c301604bee2b571c81ca35b8dcf7a6fcd272a
Orangerin <orangerin@OrangerindeMacBook-Air.local>
2026-07-21 17:19:24 +0800

    Add taobao cask

---
+++ Casks/taobao.rb
@@ -0,0 +1,11 @@
+cask "taobao" do
+  version "2.5.1"
+  sha256 "1997acf793981263bdcdc3b272d01552428321243d019d532a72d1f83471e258"
+
+  url "file:///Users/orangerin/Downloads/taobao-setup-darwin-arm64-2.5.1.dmg"
+  name "淘宝桌面版"
+  desc "淘宝桌面版客户端"
+  homepage "https://www.taobao.com"
+
+  app "淘宝桌面版.app"
+end
