366176a62769461c17bdbde30f8eb23561dee5e8
Shiorangerin <141531482+Shiorangerin@users.noreply.github.com>
2026-07-30 12:22:13 +0800

    tui-music 0.1.17 (playlist support)

--- Formula/tui-music.rb
+++ Formula/tui-music.rb
@@ -1,12 +1,13 @@
 class TuiMusic < Formula
-  v = "0.1.16"
+  v = "0.1.17"
   tag = "v#{v}"
 
   desc "Terminal music player with live FFT spectrum visualization (Rust)"
   homepage "https://github.com/Shiorangerin/tui-music"
   url "https://github.com/Shiorangerin/tui-music/archive/refs/tags/#{tag}.tar.gz"
-  sha256 "043ae763d92f8b2130eaabf720c21aab5ce17559eba5489288a87ff0c6be9682"
+  sha256 "2c59a84d98f89fe040bf9f1d3ad77fe031ea493220343d6783df397d9916bec2"
   license "MIT"
+  version v
   head "https://github.com/Shiorangerin/tui-music.git", branch: "main"
 
   depends_on "rust" => :build
